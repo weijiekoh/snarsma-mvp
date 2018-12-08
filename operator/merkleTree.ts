51139be26fc3d6f2068d14dd85e6004dd9c71ad4
laiyingtong <yingtong.lai@u.yale-nus.edu.sg>
2018-12-08 13:44:56 +0800

    Fix operatorTest.js

--- operator/merkleTree.ts
+++ operator/merkleTree.ts
@@ -1,5 +1,5 @@
 //npm i merkletreejs
-import {hashBuf, numToBuf} from '../utils/hash'
+import {numToBuf, pedersenHash} from '../utils/hash'
 import * as bigInt from 'big-integer'
 
 const MerkleTree = require('merkletreejs');
@@ -15,9 +15,9 @@ interface ILeaf {
 }
 
 //hash leaf for leaf lookup
-function leafToBuffer(leaf: ILeaf){
-    // get the first 24 bytes of leaf pubkey
-    const pubKeyForHash = leaf.pubKey.slice(0, 24)
+function leafToHash(leaf: ILeaf){
+
+    const pubKeyForHash = leaf.pubKey
 
     //Nonce is 4 bytes
     const nonceBytes = numToBuf(bigInt(leaf.nonce), 4)
@@ -25,26 +25,41 @@ function leafToBuffer(leaf: ILeaf){
     //Amount is 2 bytes
     const balanceBytes = numToBuf(bigInt(leaf.balance), 2)
     
-    // Concat everything
-    const everything = Buffer.alloc(30)
+    const everything = Buffer.alloc(38)
     for (let i = 0; i < 2; i++) {
-        everything[29-i] = balanceBytes[2-i]
+        everything[37-i] = balanceBytes[2-i]
     }
     
     for (let i = 0; i < 4; i++) {
-        everything[27-i] = nonceBytes[4-i]
+        everything[35-i] = nonceBytes[4-i]
     }
     
-    for (let i = 0; i < 24; i++) {
-        everything[23-i] = pubKeyForHash[23-i]
+    for (let i = 0; i < 32; i++) {
+        everything[31-i] = pubKeyForHash[31-i]
     }
-    return everything
+    return hashLeaf(everything)
+    // return everything
 }
 
 
 function hashLeaf(leafBuffer){
-//TODO: Change to Pedersen hash (Zcash)
-    return numToBuf(hashBuf(leafBuffer), 32)
+
+    let emptyLeafBuffer =  Buffer.allocUnsafe(38).fill(0)
+    let emptyLeafHash = Buffer.from([0x1c, 0xe7, 0xd9, 0x46, 0x19, 0x63, 0xa7, 0xbe, 0x8d, 0x31, 0x2b, 0xca, 0x60, 0x17, 0xba, 0x5c, 0xd7, 0x81, 0x5a, 0xc6, 0xd7, 0xa5, 0x2e, 0x8b, 0xdf, 0x24, 0x72, 0x0b, 0x8f, 0x7c, 0xed, 0xa5])
+    if (Buffer.compare(leafBuffer,emptyLeafBuffer)==0){
+        return emptyLeafHash
+    }
+    else{
+        return pedersenHash(leafBuffer)
+    }
+    
 }
 
-export {ILeaf, leafToBuffer, hashLeaf, MerkleTree}
+// function hashLeaf(leafBuffer){
+
+//     return pedersenHash(leafBuffer)
+    
+// }
+
+
+export {ILeaf, leafToHash, hashLeaf, MerkleTree}
